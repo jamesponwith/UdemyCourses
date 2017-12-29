213318bf7aaeba46c1d45137b1afa6d7300988fb
jamesponwith <jponwith@sandiego.edu>
2017-12-28 22:49:20 -0700

    missing data

--- MachineLearning/ImportingDataset.py
+++ MachineLearning/ImportingDataset.py
@@ -18,3 +18,9 @@ X = dataset.iloc[:, :-1].values
 # Create dependent variable martix
 
 Y = dataset.iloc[:,3].values
+
+# Taking care of missing data
+from sklearn.preprocessing import Imputer
+imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform(X[:, 1:3])
