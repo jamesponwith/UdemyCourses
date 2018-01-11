4c2ecfdc8a810d753882d31a72dc3c5bf8c07e40
jamesponwith <jponwith@sandiego.edu>
2018-01-10 22:22:58 -0700

    Handling Categorical data

--- MachineLearning/ImportingDataset.py
+++ MachineLearning/ImportingDataset.py
@@ -22,5 +22,15 @@ Y = dataset.iloc[:,3].values
 # Taking care of missing data
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values = 'NaN', strategy = 'mean', axis = 0)
-imputer = imputer.fit(X[:, 1:3])
+imputer = imputer.fit(X[:, 1:3]) # Outer bound is exclusive
 X[:, 1:3] = imputer.transform(X[:, 1:3])
+
+# Encoding catagorical data
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+labelEncoder_X = LabelEncoder()
+X[:, 0] = labelEncoder_X.fit_transform(X[:, 0])
+oneHotEncoder = OneHotEncoder(categorical_features = [0])
+X = oneHotEncoder.fit_transform(X).toarray()
+labelEncoder_Y = LabelEncoder()
+Y = labelEncoder_Y.fit_transform(Y)
+
