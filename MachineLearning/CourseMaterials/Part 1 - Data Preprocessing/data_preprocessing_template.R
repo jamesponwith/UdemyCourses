7ae61f2808eaaaa8bcc4f6744e09be978ad5b048
jamesponwith <jponwith@sandiego.edu>
2018-01-12 16:18:50 -0700

    file rearrange

--- MachineLearning/CourseMaterials/Part 1 - Data Preprocessing/data_preprocessing_template.R
+++ MachineLearning/CourseMaterials/Part 1 - Data Preprocessing/data_preprocessing_template.R
@@ -1,16 +1,19 @@
-# Data Preprocessing Template
+# Data Preprocessing
 
-# Importing the dataset
+# Importing data set
 dataset = read.csv('Data.csv')
+# dataset = dataset[, 2:3]
 
-# Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+# Splitting dataset into Training set and Testing set
+install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8) # input % towards training
 training_set = subset(dataset, split == TRUE)
-test_set = subset(dataset, split == FALSE)
+testing_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+# training_set[, 2:3] = scale(training_set[, 2:3])
+# testing_set[, 2:3] = scale(testing_set[, 2:3])
+
+
--- MachineLearning/CourseMaterials/Part 1 - Data Preprocessing/data_preprocessing_template.py
+++ MachineLearning/CourseMaterials/Part 1 - Data Preprocessing/data_preprocessing_template.py
@@ -1,23 +1,34 @@
-# Data Preprocessing Template
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 11 14:55:43 2018
 
-# Importing the libraries
+@author: jamesponwith
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
 dataset = pd.read_csv('Data.csv')
+
+# Matrix of features
 X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, 3].values
 
-# Splitting the dataset into the Training set and Test set
+# Create dependent variable martix
+Y = dataset.iloc[:,3].values
+
+# Splitting dataset into the Training set and Testing set
 from sklearn.cross_validation import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size = 0.2, random_state = 0)
 
+"""
 # Feature Scaling
-"""from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
 X_train = sc_X.fit_transform(X_train)
-X_test = sc_X.transform(X_test)
-sc_y = StandardScaler()
-y_train = sc_y.fit_transform(y_train)"""
+X_test = sc_X.fit_transform(X_test)
+
+"""
+
