626763b6470615e0fa40739e647ec92e31d2164e
jamesponwith <jponwith@sandiego.edu>
2018-01-12 18:02:53 -0700

    First complete Machine Learning Model (Simple Linear Regression)

---
+++ MachineLearning/Part 2 - Regression/Section 4 - Simple Linear Regression/SLR.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jan 12 16:46:56 2018
+
+@author: jamesponwith
+"""
+
+# Simple Linear Regression
+
+# Data Preprocessing
+# Libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# Importing the dataset
+dataset = pd.read_csv('Salary_Data.csv')
+X = dataset.iloc[:, :-1].values # X is the matrix of features (Independent variables) (Takes dataset and removes the end column(Dependent variable))
+y = dataset.iloc[:, 1].values   # 1 represents the column of the dependent variable (Y is the vector of dependent variables) (X is the matix of features)
+
+# Splitting the dataset into the Training set and Test set
+from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
+
+# Feature Scaling
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+sc_y = StandardScaler()
+y_train = sc_y.fit_transform(y_train)"""
+
+# Fitting Simple Linear Regression to the Training Set
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+# Predicting the Test set Results
+y_pred = regressor.predict(X_test) # Vector of predicitons
+
+# Visualizing Training set results
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Training Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing Test set results
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train, regressor.predict(X_train), color = 'blue')
+plt.title('Salary vs Experience (Test Set)')
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+
+
+
+
