36b4ba7775dbb0428ed538a9e8789accd09737fa
James B. Ponwith <jponwith@jamess-imac.attlocal.net>
2018-02-03 08:27:02 -0800

    BW Elim function for R

--- MachineLearning/Part 2 - Regression/Section 5 - Multiple Linear Regression/MLR.R
+++ MachineLearning/Part 2 - Regression/Section 5 - Multiple Linear Regression/MLR.R
@@ -43,3 +43,22 @@ summary(regressor)
 regressor = lm(formula = Profit ~ R.D.Spend, 
                data = dataset)
 summary(regressor)
+
+# # backwardElimination <- function(x, sl) {
+# numVars = length(x)
+# for (i in c(1:numVars)){
+#   regressor = lm(formula = Profit ~ ., data = x)
+#   maxVar = max(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"])
+#   if (maxVar > sl){
+#     j = which(coef(summary(regressor))[c(2:numVars), "Pr(>|t|)"] == maxVar)
+#     x = x[, -j]
+#   }
+#   numVars = numVars - 1
+# }
+# return(summary(regressor))
+# }
+# 
+# SL = 0.05
+# dataset = dataset[, c(1,2,3,4,5)]
+# backwardElimination(training_set, SL)
+
