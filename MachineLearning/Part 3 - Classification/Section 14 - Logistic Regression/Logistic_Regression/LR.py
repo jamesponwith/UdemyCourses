a904fca34ab642f77861d6e3ea940d9108a9e2e0
jamesponwith <jponwith@sandiego.edu>
2018-03-08 10:25:30 -0800

    Logistic Regression graphed

--- MachineLearning/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/LR.py
+++ MachineLearning/Part 3 - Classification/Section 14 - Logistic Regression/Logistic_Regression/LR.py
@@ -35,4 +35,59 @@ X_test = sc_X.transform(X_test)
 
 # Fitting Logistic Regression to the Training Set
 from sklearn.linear_model import LogisticRegression
-classifier = LogisticRegression()
+classifier = LogisticRegression(random_state = 0)
+classifier.fit(X_train, y_train) # Fit classifier object to training set to learn correlations
+
+# Predicting the test set results
+y_pred = classifier.predict(X_test) # The vector of predictions for each test set observation 
+
+# Evaluate Logistic Regression Model 
+# Make the Confusion Matrix
+from sklearn.metrics import confusion_matrix
+cm = confusion_matrix(y_test, y_pred) 
+
+# View cm by typing into console 
+# Displays matrix [[65, 3], [8, 24]] 
+# Meaning 65 + 24 correct predictions and 8 + 3 wrong predictions
+
+# Visualizing the Training set results using prediction regions
+# Region is classifier prediciton while points are the truth
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_train, y_train
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Training set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
+
+# Visualizing the Test set results 
+from matplotlib.colors import ListedColormap
+X_set, y_set = X_test, y_test
+
+# Prepare grid with pixel points
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+
+# Apply classifier to pixel points to predict if it belongs to class 1 or 0
+# contourf function creates prediciton boundry
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green'))) 
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Logistic Regression (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
